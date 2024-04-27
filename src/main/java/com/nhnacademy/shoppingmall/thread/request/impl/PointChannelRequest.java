7c905eb047d3056e705a1a416b22633dd416414d
2-say <dev2say@gmail.com>
2024-04-27 21:34:19 +0900

    feat: Add Point worker Thread #todo14

--- src/main/java/com/nhnacademy/shoppingmall/thread/request/impl/PointChannelRequest.java
+++ src/main/java/com/nhnacademy/shoppingmall/thread/request/impl/PointChannelRequest.java
@@ -11,6 +11,8 @@ public class PointChannelRequest extends ChannelRequest {
     public void execute() {
         DbConnectionThreadLocal.initialize();
         //todo#14-5 포인트 적립구현, connection은 point적립이 완료되면 반납합니다.
+
+
         log.debug("pointChannel execute");
         DbConnectionThreadLocal.reset();
     }
