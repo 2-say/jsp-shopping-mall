d1852d7b97bf6148336138d4d26896fd9eff4d32
2-say <dev2say@gmail.com>
2024-04-29 13:22:27 +0900

    feat: Product 내용 view 추가

--- src/main/java/com/nhnacademy/shoppingmall/controller/index/IndexController.java
+++ src/main/java/com/nhnacademy/shoppingmall/controller/index/IndexController.java
@@ -2,14 +2,22 @@ package com.nhnacademy.shoppingmall.controller.index;
 
 import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
 import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
+import com.nhnacademy.shoppingmall.common.page.Page;
+import com.nhnacademy.shoppingmall.product.entity.Product;
+import com.nhnacademy.shoppingmall.product.model.ProductDao;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
-@RequestMapping(method = RequestMapping.Method.GET,value = {"/index.do"})
+@RequestMapping(method = RequestMapping.Method.GET, value = {"/index.do"})
 public class IndexController implements BaseController {
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) {
+        ProductDao productDao = new ProductDao();
+        ArrayList<Product> products = productDao.list();
+        Page<Product> page = new Page<>(products, products.size());
+        req.setAttribute("page", page);
         return "shop/main/index";
     }
 }
