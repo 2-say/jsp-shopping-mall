526ca88c5f636b1666f9c034c7946ace571f343f
2-say <dev2say@gmail.com>
2024-05-01 14:45:56 +0900

    test: User Validator

---
+++ src/test/java/com/nhnacademy/shoppingmall/common/ValidationTest.java
@@ -0,0 +1,28 @@
+package com.nhnacademy.shoppingmall.common;
+
+import com.nhnacademy.shoppingmall.user.domain.User;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public class ValidationTest {
+
+    @Test
+    void beanValidation() {
+        final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+        User user = new User(" ", "  ", " " ,  " " , User.Auth.ROLE_USER, 1000, LocalDateTime.now(), null);
+
+        Set<ConstraintViolation<User>> validate = validator.validate(user);
+        for (ConstraintViolation<User> violation : validate) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation.getMessage() = " + violation.getMessage());
+        }
+
+    }
+}
